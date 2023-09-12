53f644260b226f6232799cb5b080f58cb44e3fc0
Munir Muratovic <58771762+munirmuratovic@users.noreply.github.com>
2023-09-13 01:13:30 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -10,36 +10,26 @@ from models.llms import *
 app = FastAPI()
 
 
-@app.get("/")
-def read_chat():
-    model = GPT4All("orca-mini-3b.ggmlv3.q4_0.bin")
-    output = model.generate("The capital of France is ", max_tokens=3)
-    return {"output": output}
-
-
 @app.post("/default")
 def post_chat(request):
-    model = chooseModel[request.model]
+    model = chooseModel["llama2"]
     model.chat_session(system_prompt=SYSTEM_PROMPT, prompt_template=PROMPT_TEMPLATE)
     result = model.generate(
-        prompt=format_prompt(request.messages[-1].content),
+        prompt=format_prompt(request),
         temp=0.1,
-        streaming=True,
-        max_tokens=100,
+        streaming=False,
+        max_tokens=400,
     )
 
-    message = get_message_from_stream(result)
-    response_object = create_response_object(message, request.model)
-
-    return response_object
+    return result
 
 
 @app.post("/stream")
 def post_chat(request):
-    model = chooseModel[request.model]
+    model = chooseModel["llama2"]
     model.chat_session(system_prompt=SYSTEM_PROMPT, prompt_template=PROMPT_TEMPLATE)
     result = model.generate(
-        prompt=format_prompt(request.messages[-1].content),
+        prompt=format_prompt(request),
         temp=0.1,
         streaming=True,
         max_tokens=100,
